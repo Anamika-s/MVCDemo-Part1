641e642c09417d3b4eb51d314d3fe4613b6d33d9
Anamika_S <anamika.sawhney22@gmail.com>
2022-03-08 10:15:55 +0000

    Add project files.

---
+++ MVCDemo/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            Customer customer = new Customer()
+            { Id = 1, Name = "Ajay" };
+            ViewBag.customer = customer;
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.msg = "This is a message coming from ActionMethod";
+            ViewBag.date = DateTime.Now.ToShortDateString();
+            return View();
+        }
+        public IActionResult Index1()
+        {
+            ViewData["msg"] = "This is from AM";
+            ViewData["date"] = DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
